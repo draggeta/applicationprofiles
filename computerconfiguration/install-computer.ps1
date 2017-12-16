c79657e532ee41c0d5bdec41da7ce0800d9593ec
Tony Fortes Ramos <tony.fortesramos@ogd.nl>
2017-12-16 21:32:48 +0100

    Added various options - Set the execution policy - Added Developer Mode registry key - Split the list of items to separate arrays

--- computerconfiguration/install-computer.ps1
+++ computerconfiguration/install-computer.ps1
@@ -1,20 +1,70 @@
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+
 # Trust the PowerShell Gallery
 $policy = Get-PSRepository -Name "PSGallery" | Select-Object -ExpandProperty "InstallationPolicy"
 if ($policy -ne "Trusted") {
     Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 }
 
+# Remove unneeded/unsecure Windows features
+$removedOptionalFeatureList = @(
+    'SMB1Protocol'
+    'MicrosoftWindowsPowerShellV2'
+    'MicrosoftWindowsPowerShellV2Root'
+    'WindowsMediaPlayer'
+)
+
+$enabledOptionalFeatureList = @(
+    'Microsoft-Hyper-V-All'
+    'Containers'
+    'Microsoft-Windows-Subsystem-Linux'
+)
+Disable-WindowsOptionalFeature -Online -FeatureName $removedOptionalFeatureList-NoRestart
+Enable-WindowsOptionalFeature -Online -FeatureName $enabledOptionalFeatureList -NoRestart
+
+# Create AppModelUnlock if it doesn't exist, required for enabling Developer Mode
+$RegistryKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock"
+if (-not(Test-Path -Path $RegistryKeyPath)) {
+    New-Item -Path $RegistryKeyPath -ItemType Directory -Force
+}
+
+# Add registry value to enable Developer Mode
+New-ItemProperty -Path $RegistryKeyPath -Name AllowDevelopmentWithoutDevLicense -PropertyType DWORD -Value 1
+
 # Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-# Remove unneeded/unsecure Windows features
-Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol, MicrosoftWindowsPowerShellV2, MicrosoftWindowsPowerShellV2Root, WindowsMediaPlayer -NoRestart
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All, Containers, Microsoft-Windows-Subsystem-Linux -NoRestart
-
 # Install software
+$softwareList = @(
+    'git'
+    'docker-for-windows'
+    'terraform'
+    'visualstudiocode'
+    'notepadplusplus'
+    'googlechrome'
+    'firefox'
+    'keepass'
+    '7zip'
+    'golang'
+    'microsoft-teams'
+    'putty'
+    'python'
+    'python2'
+    'wireshark'
+    'vmwarevsphereclient'
+    'rdcman'
+    'k-litecodecpackfull'
+)
 choco install -y openssh -params "/SSHAgentFeature /SSHServerFeature"
-choco install -y git, terraform, visualstudiocode, notepadplusplus, googlechrome, firefox, keepass, 7zip, golang, microsoft-teams, putty.install, python, python2, wireshark, vmwarevsphereclient, rdcman
+choco install -y $softwareList
 
 # Install often used PowerShell modules
-Install-Module -Name AzureRM, AzureAdPreview, Posh-git, PSReadLine, Pester -SkipPublisherCheck -Force
+$moduleList = @(
+    'AzureRM'
+    'AzureAdPreview'
+    'Posh-git'
+    'PSReadLine'
+    'Pester'
+)
+Install-Module -Name $moduleList -SkipPublisherCheck -Force
