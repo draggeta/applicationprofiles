9f6bdc4cc857a22584abfbba6396a3d3eab8b94b
Tony Fortes Ramos <amfortesramos@hotmail.com>
2020-05-25 02:25:14 +0200

    Fix powershell module installation

--- computerconfiguration/settings/powershell.settings.ps1
+++ computerconfiguration/settings/powershell.settings.ps1
@@ -5,3 +5,10 @@ if ($policy -ne "Trusted") {
     Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
     Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 }
+
+# Install often used PowerShell modules
+Write-Output "Installing PowerShell modules"
+
+foreach ($module in $psModuleList) {
+    Install-Module @module -Force
+}
--- computerconfiguration/settings/powershell.settings/module.unprivileged.ps1
+++
@@ -1,8 +0,0 @@
-. (Join-Path -Path $PSScriptRoot -ChildPath "../../variables/variables.ps1" -Resolve)
-
-# Install often used PowerShell modules
-Write-Output "Installing PowerShell modules"
-
-foreach ($module in $psModuleList) {
-    Install-Module @module -Force
-}
