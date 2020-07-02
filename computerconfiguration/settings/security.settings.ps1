5b481cdac07af0bd9dff9aed8fcda9ede14abe22
Tony Fortes Ramos <amfortesramos@hotmail.com>
2020-07-02 17:23:37 +0200

    Improve defender security

--- computerconfiguration/settings/security.settings.ps1
+++ computerconfiguration/settings/security.settings.ps1
@@ -9,6 +9,34 @@ foreach ($process in $controlledFolderAccessProcesses) {
     Add-MpPreference -ControlledFolderAccessAllowedApplications (Get-Command -Name $process).Source
 }
 
+# Enable blocking of Potentially Unwanted Applications
+Write-Output "Enabling blocking of PUAP"
+Set-MpPreference -PUAProtection $true
+
+# Enable Block at first sight functionality
+Write-Output "Enabling tamper protection"
+$mpRegistryKeyPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet"
+if (-not (Test-Path $mpRegistryKeyPath)) {
+    New-Item -Path $mpRegistryKeyPath -Name "DisableBlockAtFirstSeen" -Value 0 -Force
+    New-Item -Path $mpRegistryKeyPath -Name "SpynetReporting" -Value 2 -Force
+    New-Item -Path $mpRegistryKeyPath -Name "SubmitSamplesConsent" -Value 3 -Force  # 3 sends all samples
+} else {
+    Set-ItemProperty -Path $mpRegistryKeyPath -Name "DisableBlockAtFirstSeen" -Value 0
+    Set-ItemProperty -Path $mpRegistryKeyPath -Name "SpynetReporting" -Value 2
+    Set-ItemProperty -Path $mpRegistryKeyPath -Name "DSubmitSamplesConsent" -Value 3
+}
+
+# Set Defender to be more strict about blocking, but allow exceptions
+Write-Output "Set Defender to be more strict"
+$mpRegistryKeyPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\MpEngine"
+if (-not (Test-Path $mpRegistryKeyPath)) {
+    New-Item -Path $mpRegistryKeyPath -Name "DMpBafsExtendedTimeout" -Value 10 -Force
+    New-Item -Path $mpRegistryKeyPath -Name "MpCloudBlockLevel" -Value 2 -Force  # 0 = default, 1 = moderate, 2 = high, 4 = high+
+} else {
+    Set-ItemProperty -Path $mpRegistryKeyPath -Name "DMpBafsExtendedTimeout" -Value 10
+    Set-ItemProperty -Path $mpRegistryKeyPath -Name "MpCloudBlockLevel" -Value 2
+}
+
 # Enable tamper protection (cannot do it via module yet)
 Write-Output "Enabling tamper protection"
 $mpRegistryKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows Defender\Features"
@@ -21,7 +49,10 @@ Write-Output "Enabling core isolation"
 $vbsRegistryKeyPath = "HKLM:\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios"
 if (-not (Test-Path $vbsRegistryKeyPath)) {
     New-Item -Path $vbsRegistryKeyPath -Name "HypervisorEnforcedCodeIntegrity" -Value 1 -Force
-}
-else {
+} else {
     Set-ItemProperty -Path $vbsRegistryKeyPath -Name "HypervisorEnforcedCodeIntegrity" -Value 1
 }
+
+# Run Defender in a sandbox
+Write-Output "Running Defender in a sandbox"
+[System.Environment]::SetEnvironmentVariable('MP_FORCE_USE_SANDBOX', 1, [System.EnvironmentVariableTarget]::Machine)
